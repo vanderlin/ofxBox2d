4fb68565eade00b043a0d0798c97a12ba8159ca2
Vanderlin <todd@vanderlin.cc>
2011-01-14 14:55:20 -0500

    added .cpp

--- box2d demo lib 2.1.2/src/ofxBox2d/src/ofxBox2dPolygon.h
+++ box2d demo lib 2.1.2/src/ofxBox2d/src/ofxBox2dPolygon.h
@@ -9,6 +9,12 @@ class ofxBox2dPolygon {
 
 public:
 	
-	ofxBox2dPolygon() { }
-	~ofxBox2dPolygon() { }
+	//----------------------------------------
+	ofxBox2dPolygon();
+	~ofxBox2dPolygon();
+	
+	//----------------------------------------
+	void addVertex(const ofVec2f & p);
+	void addVertex(float x, float y);
+	
 };
