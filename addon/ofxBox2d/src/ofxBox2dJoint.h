ba6f8324ea74152fe5fcb30897e7b18de04054c6
Vanderlin <todd@vanderlin.cc>
2011-01-21 15:36:38 -0500

    Formatted the joint class and fixed example

--- addon/ofxBox2d/src/ofxBox2dJoint.h
+++ addon/ofxBox2d/src/ofxBox2dJoint.h
@@ -7,38 +7,41 @@ class ofxBox2dJoint {
 	
 public:
 	
-	b2World* world;
-	b2DistanceJoint* joint;
-	int jointType;
-	bool alive;
+	b2World			*	world;
+	b2DistanceJoint *	joint;
+	int					jointType;
+	bool				alive;
 	
+	//----------------------------------------
 	ofxBox2dJoint();
 	ofxBox2dJoint(b2World* b2world, b2Body* body1, b2Body* body2, float frequencyHz=4.f, float damping=.5f, bool bCollideConnected=true);
 	ofxBox2dJoint(b2World* b2world, b2Body* body1, b2Body* body2, b2Vec2 anchor1, b2Vec2 anchor2, float frequencyHz=4.f, float damping=.5f, bool bCollideConnected=true);
 	
+	//----------------------------------------
 	void setWorld(b2World * w);
-	
 	void setup(b2World* b2world, b2Body* body1, b2Body* body2, float frequencyHz=4.f, float damping=.5f, bool bCollideConnected=true);
 	void setup(b2World* b2world, b2Body* body1, b2Body* body2, b2Vec2 anchor1, b2Vec2 anchor2, float frequencyHz=4.f, float damping=.5f, bool bCollideConnected=true);
 	
+	//----------------------------------------
 	bool isSetup();
-	
 	void draw();
 	void destroy();
 	
+	//----------------------------------------
 	/// Manipulating the length can lead to non-physical behavior when the frequency is zero.
-	void setLength(float $len);
+	void  setLength(float len);
 	float getLength();
 	
-	void setFrequency(float $freq);
+	void  setFrequency(float freq);
 	float getFrequency();
 	
-	void setDamping(float $ratio);
+	void  setDamping(float ratio);
 	float getDamping();
 	
+	//----------------------------------------
 	ofVec2f getReactionForce(float inv_dt) const;
-	b2Vec2 getReactionForceB2D(float inv_dt) const;
-	float getReactionTorque(float inv_dt) const;
+	b2Vec2  getReactionForceB2D(float inv_dt) const;
+	float  getReactionTorque(float inv_dt) const;
 };
 
 
