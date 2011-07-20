3b9108a370a30be2d807869803b67b84aaaec61e
Vanderlin <todd@vanderlin.cc>
2011-07-20 17:33:16 -0400

    two 007 exmaples: Simple Example ofxBox2d Example

---
+++ Simple Example/src/testApp.cpp
@@ -0,0 +1,104 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup() {
+	
+	ofSetVerticalSync(true);
+	ofBackgroundHex(0xfdefc2);
+	ofSetLogLevel(OF_LOG_NOTICE);
+	
+	box2d.init();
+	box2d.setGravity(0, 10);
+	box2d.createBounds();
+	box2d.setFPS(30.0);
+	box2d.registerGrabbing();
+	
+	
+}
+
+//--------------------------------------------------------------
+void testApp::update() {
+	box2d.update();	
+}
+
+
+//--------------------------------------------------------------
+void testApp::draw() {
+	
+	
+	for(int i=0; i<circles.size(); i++) {
+		ofFill();
+		ofSetHexColor(0xf6c738);
+		circles[i].draw();
+	}
+	
+	for(int i=0; i<boxes.size(); i++) {
+		ofFill();
+		ofSetHexColor(0xBF2545);
+		boxes[i].draw();
+	}
+
+	// draw the ground
+	box2d.drawGround();
+	
+	
+	
+	string info = "";
+	info += "Press [c] for circles\n";
+	info += "Press [b] for blocks\n";
+	info += "Total Bodies: "+ofToString(box2d.getBodyCount())+"\n";
+	info += "Total Joints: "+ofToString(box2d.getJointCount())+"\n\n";
+	info += "FPS: "+ofToString(ofGetFrameRate(), 1)+"\n";
+	ofSetHexColor(0x444342);
+	ofDrawBitmapString(info, 30, 30);
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key) {
+	
+	if(key == 'c') {
+		float r = ofRandom(4, 20);		// a random radius 4px - 20px
+		ofxBox2dCircle circle;
+		circle.setPhysics(3.0, 0.53, 0.1);
+		circle.setup(box2d.getWorld(), mouseX, mouseY, r);
+		circles.push_back(circle);
+	}
+	
+	if(key == 'b') {
+		float w = ofRandom(4, 20);	
+		float h = ofRandom(4, 20);	
+		ofxBox2dRect rect;
+		rect.setPhysics(3.0, 0.53, 0.1);
+		rect.setup(box2d.getWorld(), mouseX, mouseY, w, h);
+		boxes.push_back(rect);
+	}
+	
+	if(key == 't') ofToggleFullscreen();
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key) {
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ) {
+	
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button) {
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button) {
+	
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button) {
+}
+
+//--------------------------------------------------------------
+void testApp::resized(int w, int h){
+}
+
