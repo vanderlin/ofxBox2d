ed6c7085b8407e6f1078a4ca5918bd64233ae27b
Vanderlin <todd@vanderlin.cc>
2011-07-20 17:33:16 -0400

    two 007 exmaples: Simple Example ofxBox2d Example

---
+++ ofxBox2dExample/src/testApp.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "ofMain.h"
+#include "ofxBox2d.h"
+
+// ------------------------------------------------- a simple extended box2d circle
+class CustomParticle : public ofxBox2dCircle {
+	
+public:
+	CustomParticle() {
+	}
+	ofColor color;
+	void draw() {
+		float radius = getRadius();
+		
+		glPushMatrix();
+		glTranslatef(getPosition().x, getPosition().y, 0);
+		
+		ofSetColor(color.r, color.g, color.b);
+		ofFill();
+		ofCircle(0, 0, radius);	
+		
+		glPopMatrix();
+		
+	}
+};
+
+
+// -------------------------------------------------
+class testApp : public ofBaseApp {
+	
+public:
+	
+	void setup();
+	void update();
+	void draw();
+	
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void resized(int w, int h);
+	
+	float							px, py;
+	bool							bDrawLines;
+	bool							bMouseForce;
+	
+	ofxBox2d						box2d;			  //	the box2d world
+	ofPolyline						drawing;		  //	we draw with this first
+	ofxBox2dPolygon					polyLine;		  //	the box2d polygon/line shape
+	vector		<ofxBox2dCircle>	circles;		  //	default box2d circles
+	vector		<ofxBox2dRect>		boxes;			  //	defalut box2d rects
+	vector		<CustomParticle>	customParticles;  //	this is a custom particle the extends a cirlce
+	
+	
+};
