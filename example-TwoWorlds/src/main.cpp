0b138c0ac654534744d0a94335300d45838bc3f3
Andy Duplain <andy@trojanfoe.com>
2015-04-06 14:14:26 +0100

    Revert "Fix locations of openFrameworks and remove use of GLUT in example-TwoWorlds."
    This reverts commit d251e5b69153c8da917813055f3bdd39127b602a.

--- example-TwoWorlds/src/main.cpp
+++ example-TwoWorlds/src/main.cpp
@@ -4,6 +4,13 @@
 
 //========================================================================
 int main( ){
-    ofSetupOpenGL(1000, 500, OF_WINDOW);
-    ofRunApp(new testApp());
+
+    ofAppGlutWindow window;
+	ofSetupOpenGL(&window, 1000, 500, OF_WINDOW);			// <-------- setup the GL context
+
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp( new testApp());
+
 }
